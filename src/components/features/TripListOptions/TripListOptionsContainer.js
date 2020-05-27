06540877231c540efbb4a760522760ceb6fc7485
Katarzyna <kkazimierczuk@gmail.com>
2020-05-27 22:29:47 +0200

    add trip list filters

---
+++ src/components/features/TripListOptions/TripListOptionsContainer.js
@@ -0,0 +1,20 @@
+import {connect} from 'react-redux';
+import TripListOptions from './TripListOptions';
+import {getAllTags} from '../../../redux/tagsRedux';
+import {getAllFilters, changeSearchPhrase, changeDurationFrom, changeDurationTo, addTag, removeTag} from '../../../redux/filtersRedux';
+
+const mapStateToProps = state => ({
+  tags: getAllTags(state),
+  filters: getAllFilters(state),
+});
+
+const mapDispatchToProps = dispatch => ({
+  changeSearchPhrase: phrase => dispatch(changeSearchPhrase(phrase)),
+  // TODO - add more dispatchers for other filters
+  changeDurationFrom: (type,value) => dispatch(changeDurationFrom(type,value)),
+  changeDurationTo: (type,value) => dispatch(changeDurationTo(type,value)),
+  addTag: (tag) => dispatch(addTag(tag)),
+  removeTag: (tag) => dispatch(removeTag(tag)),
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(TripListOptions);
